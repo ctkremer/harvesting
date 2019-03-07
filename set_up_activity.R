7c5a869ec5e01b9837ca558c606f550e920462f0
ctkremer <colin.kremer@yale.edu>
2019-03-07 09:53:42 -0500

    instructions for starting shiny app for lab activity

---
+++ set_up_activity.R
@@ -0,0 +1,17 @@
+
+# Script for initializing population regulation activity
+# UConn Waterbury Ecology class, March 2019
+
+
+# install any missing packages
+install.packages(c('ggplot2','gridExtra','deSolve','shiny'))
+
+# load libraries
+library(ggplot2)
+library(gridExtra)
+library(deSolve)
+library(shiny)
+
+# run Shiny app
+runGitHub("harvesting","ctkremer")
+
